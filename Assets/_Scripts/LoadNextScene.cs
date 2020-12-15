24f969ff4358724f63bf089bf41b920a9c843e0f
Eisen <mister.eisen@gmail.com>
2020-12-14 23:09:39 -0500

    Added Different Scenes

---
+++ Assets/_Scripts/LoadNextScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextScene : MonoBehaviour
+{
+
+    public void NextScene(string scene_name)
+        {
+            SceneManager.LoadScene(1);
+        }
+
+    }
+
